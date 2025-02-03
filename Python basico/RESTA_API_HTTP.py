a23e9ed87cf2e26fc1c8cd25b62132049f102fbb
RicardoNunes <ricardonunessergionunes@gmail.com>
2025-02-03 17:31:53 +0000

    varios assuntos

---
+++ Python basico/RESTA_API_HTTP.py
@@ -0,0 +1,22 @@
+import requests
+
+url="https://pt.wikipedia.org/wiki/Wiki" 
+
+
+r=requests.get(url) #pedido get ao servidor
+
+print(r.status_code)
+
+print(r.request.headers)
+
+print(r.request.body)
+
+print(r.url)
+
+#print(r.text) # resposta do servidor
+
+headers=r.headers
+print(headers["date"])
+print(headers["Content-type"])
+
+
